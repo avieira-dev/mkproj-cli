10ffb04a480cdae3f154f36648988abb641f8676
Alexandre Vieira <200545664+avieira-dev@users.noreply.github.com>
2026-04-15 12:37:54 -0300

    style(utils): add background colors, dim and italic support

--- utils/colors.py
+++ utils/colors.py
@@ -1,10 +1,17 @@
 class Colors:
-    HEADER = '\033[95m'
     BLUE = '\033[94m'
     CYAN = '\033[96m'
     GREEN = '\033[92m'
     YELLOW = '\033[93m'
     RED = '\033[91m'
+    PURPLE = '\033[95m'
     BOLD = '\033[1m'
+    DIM = '\033[2m'
+    ITALIC = '\033[3m'
     UNDERLINE = '\033[4m'
     END = '\033[0m'
+    
+    # Background for badges
+    BG_BLUE = '\033[44m'
+    BG_CYAN = '\033[46m'
+    WHITE = '\033[97m'
