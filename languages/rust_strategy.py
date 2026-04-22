b2f4dcf0b5dcbe67e77a5a15487bbff138a69235
Alexandre Vieira <200545664+avieira-dev@users.noreply.github.com>
2026-04-22 14:25:44 -0300

    feat(lang/rust): implement rust strategy with crate name sanitization

--- languages/rust_strategy.py
+++ languages/rust_strategy.py
@@ -1,56 +1,24 @@
 import os
-from utils.file_system import create_directory, create_file
+from utils.file_system import create_directory, create_from_template
 
-GITIGNORE_CONTENT = """# Build 
-/target/
-
-# Compiler temporary files
-**/*.rs.bk
-
-# IDE / Editor
-.vscode/
-.idea/
-*.vcxproj*
-*.sln
-*.user
-*.filters
-
-# OS
-*~
-.DS_Store
-Thumbs.db
-.cache/
-"""
-
-MAIN_CONTENT = """fn main() {
-    println!("Hello from Rust!");
-}
-"""
-
-def setup_rust(name, readme_title, readme_description):
-    # Replace spaces and hyphens with underscores and convert to lowercase
+def setup_rust(name, readme_title, readme_description, username, email, license):
     name_cargo = name.replace(" ", "_").replace("-", "_").lower()
 
-    # Folders
+    # Crete directory
     create_directory(os.path.join(name, "src"))
 
-    cargo_toml = f"""[package]
-name = "{name_cargo}"
-version = "0.1.0"
-edition = "2021"
+    # Data to fill in the templates
+    context = {
+        "name_project": name_cargo,
+        "author_info": f"{username} <{email}>",
+        "description": readme_description,
+        "license": license,
+        "readme_title": readme_title,
+        "readme_description": readme_description
+    }
 
-[dependencies]
-"""
-    
-    readme_content = f"""# {readme_title}
-
-> {readme_description}
-
-## Table of Contents
-"""
-
-    # Files
-    create_file(os.path.join(name, ".gitignore"), GITIGNORE_CONTENT)
-    create_file(os.path.join(name, "README.md"), readme_content)
-    create_file(os.path.join(name, "Cargo.toml"), cargo_toml)
-    create_file(os.path.join(name, "src", "main.rs"), MAIN_CONTENT)
+    # Create files
+    create_from_template("rust/rust_cargo.txt", os.path.join(name, "Cargo.toml"), context)
+    create_from_template("rust/rust_gitignore.txt", os.path.join(name, ".gitignore"), context)
+    create_from_template("rust/rust_readme.txt", os.path.join(name, "README.md"), context)
+    create_from_template("rust/rust_main.txt", os.path.join(name, "src", "main.rs"), context)
